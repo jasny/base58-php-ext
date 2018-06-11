db9092e373d9c1dfe3d792a2a384ce2a2ed88bc1
Michael Burns <mburns83@yahoo.com>
2018-06-11 16:20:45 -0700

    Changes to match local build steps.

--- .appveyor/build.cmd
+++ .appveyor/build.cmd
@@ -1,15 +1,54 @@
 @echo off
 
 setlocal enableextensions enabledelayedexpansion
-	set SDK_RUNNER=%PHP_BUILD_CACHE_SDK_DIR%\phpsdk-%PHP_BUILD_CRT%-%ARCHITECTURE%.bat
-	if not exist "!SDK_RUNNER!" (
-		echo "!SDK_RUNNER!" doesn't exist
-		exit /b 3
-	)
 
-	cmd /c !SDK_RUNNER! -t %APPVEYOR_BUILD_FOLDER%\.appveyor\build_task.cmd
+	REM pull and build bitcoin/libbase58
+	
+	echo Cloning Bitcoin libbase58 repository ...
+	git clone -q --depth=1 https://github.com/bitcoin/libbase58.git C:\projects\libbase58
+	pushd C:\projects\libbase58
+	git fetch origin pull/7/head:holland-windows-build
+	git checkout holland-windows-build
 
-	if %errorlevel% neq 0 exit /b 3
+	echo Building Bitcoin libbase58 ...
+	cl /W4 /c base58.c
+	lib /out:libbase58.lib base58.obj
+	popd
+
+	REM set up PHP
+	mkdir C:\projects\php-sdk >NUL 2>NUL
+	cd C:\projects\php-sdk
+    wget %PHP_SDK_BINARY_TOOLS_URL%/%PHP_SDK_BINARY_TOOLS_PACKAGE% --no-check-certificate -q -O %PHP_SDK_BINARY_TOOLS_PACKAGE%
+    7z x -y %PHP_SDK_BINARY_TOOLS_PACKAGE%
+	bin\phpsdk_buildtree.bat phpdev
+	pushd phpdev
+	ren vc9 vc14
+	pushd vc14\x64
+    git clone https://git.php.net/repository/php-src.git
+	cd php-src
+	git checkout PHP-%PHP_REL%
+	cd ..
+	wget %PHP_DEPS_URL%/%PHP_DEPS_PACKAGE% --no-check-certificate -q -O %PHP_DEPS_PACKAGE%
+	7z x -y %PHP_DEPS_PACKAGE%
+	popd
+
+	REM copy the extension into the PHP tree
+	mkdir c:\projects\php-sdk\phpdev\php-src\ext\base58
+	xcopy c:\projects\base58-php-exe\*.* c:\projects\php-sdk\phpdev\php-src\ext\base58 /s/e/v
+	
+	REM perform the build
+	bin\phpsdk_setvars.bat
+	pushd phpdev\vc14\x64\php-src
+	buildconf
+	configure --disable-all --enable-cli --with-extra-includes=c:\projects\libbase58 --with-extra-libs=c:\projects\libbase58 --with-base58=shared
+	nmake
+
+
+	REM TODO:debugging
+	dir php_base58.dll /s
+	dir php.exe /s
+	dir php*.dll /s
+
 endlocal
 
 exit /b 0
