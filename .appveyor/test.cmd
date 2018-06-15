84a6ab3133cf106ae159d57c6ef91a943a9d0442
Michael Burns <mburns83@yahoo.com>
2018-06-15 13:12:34 -0700

    Add generation of php.ini so tests are not skipped.

--- .appveyor/test.cmd
+++ .appveyor/test.cmd
@@ -2,7 +2,15 @@
 
 setlocal enableextensions enabledelayedexpansion
 
+    cd C:\projects\php-sdk\phpdev\vc14\x64\php-src
+	
+	pushd ext\base58
+    echo [PHP] > php.ini
+    echo extension_dir = "ext" >> php.ini
+    echo extension=php_base58.dll >> php.ini
+	popd
+
 	set TEST_PHP_EXECUTABLE=C:\projects\php-sdk\phpdev\vc14\x64\php-src\x64\Release_TS\php.exe
-	%TEST_PHP_EXECUTABLE% C:\projects\php-sdk\phpdev\vc14\x64\php-src\run-tests.php C:\projects\php-sdk\phpdev\vc14\x64\php-src\ext\base58 -q --show-diff
+	%TEST_PHP_EXECUTABLE% run-tests.php ext\base58 -q --show-diff
 
 endlocal
