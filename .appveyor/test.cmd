b0b5c621aa17af7d17965193dcdcccab47d5b30e
Arnold Daniels <arnold@jasny.net>
2019-06-24 14:47:00 +0200

    Revert "Replace wget for Invoke-WebRequest for AppVeyor"
    This reverts commit 93c8563bf075309d46f52c5a5c63c37190e8cab0.

--- .appveyor/test.cmd
+++ .appveyor/test.cmd
@@ -2,12 +2,12 @@
 
 setlocal enableextensions enabledelayedexpansion
 
-	cd C:\projects\php-sdk\phpdev\vc14\x64\php-src
+    cd C:\projects\php-sdk\phpdev\vc14\x64\php-src
 	
 	pushd ext\base58
-	echo [PHP] > php.ini
-	echo extension_dir = "ext" >> php.ini
-	echo extension=php_base58.dll >> php.ini
+    echo [PHP] > php.ini
+    echo extension_dir = "ext" >> php.ini
+    echo extension=php_base58.dll >> php.ini
 	popd
 
 	set TEST_PHP_EXECUTABLE=C:\projects\php-sdk\phpdev\vc14\x64\php-src\x64\Release_TS\php.exe
